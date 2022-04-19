7f7d3799c2738eb8af8cfc882af79d14de121559
Tururocks101! <truphosa.ambiyo@student.moringaschool.com>
2022-04-19 12:39:10 +0300

    Add profile

--- user/models.py
+++ user/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=30)
+    image = models.ImageField(default='default.png',
+                              upload_to='profile_images')
+
+    def __str__(self):
+        return f'{self.username}-Profile'
