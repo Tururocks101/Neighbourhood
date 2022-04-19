c3dd303e64d6ee2d071837828899d413e4c8323e
Tururocks101! <truphosa.ambiyo@student.moringaschool.com>
2022-04-19 11:11:20 +0300

    Add models

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Hood, Business, User
 
 # Register your models here.
+
+admin.site.register(Hood)
+admin.site.register(Business)
+admin.site.register(User)
--- hood/models.py
+++ hood/models.py
@@ -1,4 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Hood(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    location = models.CharField(max_length=30, null=True)
+    occupant_count = models.PositiveIntegerField(null=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Business(models.Model):
+    business_name =models.ForeignKey( on_delete=models.CASCADE)
+    owner =models.ForeignKey(User, models.CASCADE)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.business_name}'
+
+
+class User(models.Model):
+    name =models.CharField( max_length= 150)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.name}'
