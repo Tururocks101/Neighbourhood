ec5ee9840b9403f8ba486d7696a76a9ff7ff4c34
Tururocks101! <truphosa.ambiyo@student.moringaschool.com>
2022-04-19 09:56:35 +0300

    Add navbar

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from user import views as user_view
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', include('hood.urls') ),
+    path('', user_view.register, name='user-register'),
+    path('profile', user_view.profile, name='user-profile'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'user/login.html'), name = 'user-login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'user/logout.html'), name ='user-logout'),
 ]
