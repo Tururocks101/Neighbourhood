d1cf5c74cc70320c9aabc45220fb7000ae22745f
Tururocks101! <truphosa.ambiyo@student.moringaschool.com>
2022-04-19 11:22:35 +0300

    Add login

--- hood/views.py
+++ hood/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+
+
+@login_required(login_url='user-login')
+def index(request):
+    return render(request, 'dashboard/index.html')
+
+@login_required(login_url='user-login')
+def user(request):
+    return render(request, 'dashboard/user.html')   
+
+@login_required(login_url='user-login')
+def business(request):
+    return render(request, 'dashboard/business.html')
+
+@login_required(login_url='user-login')
+def neighbourhood(request):
+    return render(request, 'dashboard/neighbourhood.html')    
